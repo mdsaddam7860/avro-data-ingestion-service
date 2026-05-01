22a7f83fb3663053b2b2ff0b0784d31dd1019fa9
Md Saddam <mdsonu7860@outlook.com>
2026-05-01 16:45:30 +0530

    Deploy

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -270,28 +270,36 @@ function modifyData(jsonData, heapTableName2 = null) {
   let modifiedData = null;
 
   if (heapTableName2 === "users" && jsonData.length > 0) {
-    modifiedData = jsonData.filter(
-      (item) => item && item.user_email && item.company_name
-    );
-    const payload1 = modifiedData.map((item) => ({
-      name: item.user_full_name,
-      sourceId: item.company_sfdcid,
-    }));
+    const payload1 = jsonData
+      .filter((item) => item && item?.company_sfdcid && item?.company_name)
+      .map((item) => ({
+        name: item.company_name,
+        sourceId: item.company_sfdcid,
+      }));
 
-    const payload2 = modifiedData.map((item) => {
-      // 1. Extract first Name and last Name
-      const { firstName, lastName } = extractNames(item?.user_full_name);
+    const payload2 = jsonData
+      .filter(
+        (item) =>
+          item &&
+          item?.user_email &&
+          item?.company_name &&
+          item?.company_sfdcid &&
+          item?.user_id
+      )
+      .map((item) => {
+        // 1. Extract first Name and last Name
+        const { firstName, lastName } = extractNames(item?.user_full_name);
 
-      // 2. Explicitly return the new object
-      return {
-        firstName,
-        lastName,
-        companyId: `srcid-${item.company_sfdcid}`,
-        email: item.user_email,
-        // position: item.user_email,
-        externalId: String(item.user_id),
-      };
-    });
+        // 2. Explicitly return the new object
+        return {
+          firstName,
+          lastName,
+          companyId: `srcid-${item.company_sfdcid}`,
+          email: item.user_email,
+          // position: item.user_email,
+          externalId: String(item.user_id),
+        };
+      });
 
     const data = {
       Results1: payload1,
